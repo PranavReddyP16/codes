b83896673bcf25f32a25692228a8f2ef46ed6f83
PranavReddyP16 <bestpranav16@gmail.com>
2019-10-07 14:12:18 +0530

    changed value of m

--- sn.c
+++ sn.c
@@ -1,49 +1,45 @@
 #include<stdio.h>
 
-const int m = 1e3;
+const long long int m = 1e3;
 
 int main()
 {
 	int n,k;
-	scanf("%d",&n);
-	scanf("%d",&k);
-
-
+	scanf("%d %d",&n,&k);
+    int i;
 	int a[m];
+    for(i=0;i<m;i++)
+    {
+        a[i] = 0;
+    }
 	int b[k];
-	int x,i;
+    for(i=0;i<k;i++)
+    {
+        b[i] = 0;
+    }
+	int x;
 	
 
-	int j = k-1;
+	int j=k-1;
 
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&x);
-		
-	
-		
+
 		if(a[x-1] == 0)
 		{
 			b[j] = x;
-			a[x-1]  = 1;
+			a[x]  = 1;
 			j = j - 1;
 
 		}
-		
-	
 	}
 
 	for(i=0;i<k;i++)
 	{
 		if(b[i]!=0)
-		{
 			printf("%d ",b[i]);
-			
-		}
-
 	}
-	
-	
 
 
 
